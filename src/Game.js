01ca90777799c4270b6b4f59087a35f881ee0809
codeComet <unknownmind10@gmail.com>
2022-02-27 15:37:20 +0600

    game completed

--- src/Game.js
+++ src/Game.js
@@ -5,7 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 const Game = () => {
   const [gameState, setGameState] = React.useState("X");
-  const [cellValue, setCellValue] = React.useState(Array(9).fill(""));
+  const [cellValue, setCellValue] = React.useState(Array(16).fill(""));
   const [winner, setWinner] = React.useState("");
 
   const handleClick = (id) => {
@@ -17,11 +17,20 @@ const Game = () => {
         checkWinner(newArray);
         return newArray;
       });
+      console.log(id);
     } else {
       alert("Already filled");
       return;
     }
-    setGameState(gameState === "X" ? "O" : "X");
+    setGameState(
+      gameState === "X"
+        ? "O"
+        : gameState === "O"
+        ? "#"
+        : gameState === "#"
+        ? "X"
+        : ""
+    );
   };
 
   //toast
@@ -39,28 +48,36 @@ const Game = () => {
   const checkWinner = (arr) => {
     const winningCombinations = {
       side: [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
+        [0, 1, 2, 3],
+        [4, 5, 6, 7],
+        [8, 9, 10, 11],
+        [12, 13, 14, 15],
       ],
       down: [
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
+        [0, 4, 8, 12],
+        [1, 5, 9, 13],
+        [2, 6, 10, 14],
+        [3, 7, 11, 15],
       ],
       diagonal: [
-        [0, 4, 8],
-        [2, 4, 6],
+        [0, 5, 10, 15],
+        [3, 6, 9, 12],
       ],
     };
 
     for (let combo in winningCombinations) {
       winningCombinations[combo].forEach((item) => {
-        if (arr[item[0]] === "" || arr[item[1]] === "" || arr[item[2]] === "") {
+        if (
+          arr[item[0]] === "" ||
+          arr[item[1]] === "" ||
+          arr[item[2]] === "" ||
+          arr[item[3]] === ""
+        ) {
           return;
         } else if (
           arr[item[0]] === arr[item[1]] &&
-          arr[item[1]] === arr[item[2]]
+          arr[item[1]] === arr[item[2]] &&
+          arr[item[2]] === arr[item[3]]
         ) {
           setWinner(arr[item[0]]);
           notify(arr[item[0]]);
@@ -100,16 +117,25 @@ const Game = () => {
             <Box id="0" />
             <Box id="1" />
             <Box id="2" />
+            <Box id="3" />
           </tr>
           <tr>
-            <Box id="3" />
             <Box id="4" />
             <Box id="5" />
-          </tr>
-          <tr>
             <Box id="6" />
             <Box id="7" />
+          </tr>
+          <tr>
             <Box id="8" />
+            <Box id="9" />
+            <Box id="10" />
+            <Box id="11" />
+          </tr>
+          <tr>
+            <Box id="12" />
+            <Box id="13" />
+            <Box id="14" />
+            <Box id="15" />
           </tr>
         </tbody>
       </table>
